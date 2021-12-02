6bd25d0f365bd624a6cc7449f6c317b29bbfb410
dylan <dylan@86degrees.com>
2021-12-02 14:53:38 +0200

    launchAutoHide false

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,6 +1,6 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
 /* tslint:disable */
 export const VERSION = {
-    "date": "2021/12/02, 14:00:30"
+    "date": "2021/12/02, 14:43:56"
 };
 /* tslint:enable */
